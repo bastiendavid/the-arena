847f9282a3b4ebf22a538963fd2dc70d4cc3b729
Bastien DAVID <bastien.david@orange.com>
2015-09-30 12:18:39 +0200

    Mini chat implementation, to test socket.io

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 'use strict'
 
 var express = require('express');
-
 var server = express();
+var http = require('http').Server(server);
+var io = require('socket.io')(http);
 
 // Serve static content
 server.use(express.static(__dirname + '/frontend'));
+server.use("/jquery", express.static(__dirname + '/node_modules/jquery/dist'));
+
+
+// sockets
+io.on('connection', function(socket){
+    console.log('a user connected');  
+    socket.on('user', function(username) {
+        io.emit('message', username + ' entered the room');
+    })
+    
+    socket.on('message', function(message) {
+        io.emit('message', message);
+    });
+});
 
 // start server
 var port = process.env.PORT || 8080;
-server.listen(port, "0.0.0.0", function () {
+http.listen(port, function () {
     console.log('Server started on port ' + port);
 });
